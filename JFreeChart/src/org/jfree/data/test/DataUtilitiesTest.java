39585f37e44987e1bfa945805097533ddce8d3e4
Jathniel Ong <ong.jathniel@gmail.com>
2020-02-05 14:13:15 -0700

    Added Test File for DataUtilities
    Created Java Unit Test for DataUtilitiesTest

---
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -0,0 +1,34 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataUtilitiesTest {
+
+	@Test
+	public void testCalculateColumnTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateRowTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNumberArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNumberArray2D() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCumulativePercentages() {
+		fail("Not yet implemented");
+	}
+
+}
